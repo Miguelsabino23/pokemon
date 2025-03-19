6dbc4e9dd50ebd98ae85fca09197214c12734786
miguelsabino23 <miguel2012palhada@gmail.com>
2025-03-19 17:32:47 -0300

    feat: configura o roteamento com react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
+import { Route, Routes } from "react-router";
+import { Layout } from "./layout";
+import { Home } from "./pages/home";
+import { Pokedex } from "./pages/pokedex";
+import { Legendaries } from "./components/legendaries";
+
 export function App() {
-  return <div>teste</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='pokedex' element={<Pokedex />} />
+        <Route path='legendaries' element={<Legendaries />} />
+      </Route>
+    </Routes>
+  );
 }
